0f765dbcd2dcfb8714b23cbeb484a96a34b90148
Dan McCahon <73975721+dtoshm@users.noreply.github.com>
2023-11-08 13:35:27 +0800

    0.3 - Step 6
    Initialized inventory database. Normally I would create a plan in comments first, however per Megan's advice I created 2 other practice databases in between each course section and was feeling eager for this project.

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ from models import (Base, session,
 import datetime
 import csv
 import time
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import (create_engine, Column, 
+                        Integer, String, Date)
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///inventory.db', echo=False)
+Session = sessionmaker(engine)
+session = Session()
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = "books"
+    
+    id = Column(Integer, primary_key=True)
+    product_name = Column('product_name', String)
+    product_price = Column('product_price', Integer)
+    product_quantity = Column('product_quantity', Integer)
+    date_updated = Column('date_updated', Date)
+    
+    def __repr__(self):
+        return f'''product_name: {self.product_name}, 
+                   \rproduct_price: {self.product_price}, 
+                   \rproduct_quantity: {self.product_quantity}, 
+                   \rdate_updated: {self.date_updated}'''
